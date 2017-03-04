75ceb3b0f7d2dbd93db794daab807e955e065c05
pegars@gmail.com <pegars>
2017-03-04 20:26:24 +0100

    Initial import.

---
+++ rpi-thermostat/src/main/java/com/pegars/rpi/thermostat/RpiThermostatApplication.java
@@ -0,0 +1,12 @@
+package com.pegars.rpi.thermostat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpiThermostatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpiThermostatApplication.class, args);
+	}
+}
---
+++ rpi-thermostat/src/test/java/com/pegars/rpi/thermostat/RpiThermostatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pegars.rpi.thermostat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RpiThermostatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
