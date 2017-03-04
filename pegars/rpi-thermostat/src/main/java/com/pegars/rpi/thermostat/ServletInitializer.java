52c09b93814785b8fe1943dccdbed21f3b3d78d4
pegars@gmail.com <pegars>
2017-03-04 19:55:41 +0100

    Initial import

---
+++ pegars/rpi-thermostat/src/main/java/com/pegars/rpi/thermostat/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.pegars.rpi.thermostat;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RpiThermostatApplication.class);
+	}
+
+}
